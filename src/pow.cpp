819eced58dc5c479664ffed85af9f1ad124ef288
Hellar <hellarcoin@gmail.com>
2022-06-02 19:11:13 +0300

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -81,7 +81,7 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const Conse
 }
 
 unsigned int static DarkGravityWave(const CBlockIndex* pindexLast, const Consensus::Params& params) {
-    /* current difficulty formula, hellar - DarkGravity v3, written by Evan Duffield - evan@hellar.io */
+    /* current difficulty formula, hellar - DarkGravity v3, written by Evan Duffield - evan@dash.org */
     const CBlockIndex *BlockLastSolved = pindexLast;
     const CBlockIndex *BlockReading = pindexLast;
     int64_t nActualTimespan = 0;
